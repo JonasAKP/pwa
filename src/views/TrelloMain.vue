7c38525f5353afda80d5ee06025ca52aecac0e4e
MikkelFerrari <mikkelferrari123@gmail.com>
2021-04-14 10:18:42 +0200

    draggable done

--- src/views/TrelloMain.vue
+++ src/views/TrelloMain.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="container mt-5">
         <div class="row">
-            <div class="col form-inline">
-            <v-input dark v-model="newTask" placeholder="Enter Task" v-on:keyup.enter="add"></v-input> 
+            <div class="col form-inline" style="padding-left:0px">
+            <input style="border:black solid 1px" v-model="newTask" placeholder="Enter Task" v-on:keyup.enter="add">
+            <v-btn style="height: 28px; margin: 10px" color="primary" @click="add">New task</v-btn> 
             </div>
           </div>
-          <div class="row">
-              <div class="col-md-3">
+          <div class="row mt-3">
+              <v-card class="col-md-2">
                   <div class="p-2 alert alert-secondary">
                 <h3>Backlog</h3>
                 <draggable class="list-group" :list="arrBacklog" group="tasks">
@@ -15,7 +16,43 @@
                 </div>
                 </draggable>
                 </div>
-            </div>
+            </v-card>
+
+
+             <v-card class="col-md-2">
+                  <div class="p-2 alert alert-primary">
+                <h3>In progress</h3>
+                <draggable class="list-group" :list="arrInProgress" group="tasks">
+                <div class="list-group-items" v-for="element in arrInProgress" :key="element.name">
+                    {{element.name}}
+                </div>
+                </draggable>
+                </div>
+            </v-card>
+
+
+             <v-card class="col-md-2">
+                  <div class="p-2 alert alert-secondary">
+                <h3>Tested</h3>
+                <draggable class="list-group" :list="arrTested" group="tasks">
+                <div class="list-group-items" v-for="element in arrTested" :key="element.name">
+                    {{element.name}}
+                </div>
+                </draggable>
+                </div>
+            </v-card>
+
+
+             <v-card class="col-md-2">
+                  <div class="p-2 alert alert-secondary">
+                <h3>Done</h3>
+                <draggable class="list-group" :list="arrDone" group="tasks">
+                <div class="list-group-items" v-for="element in arrDone" :key="element.name">
+                    {{element.name}}
+                </div>
+                </draggable>
+                </div>
+            </v-card>
         </div>
     </div>
 </template>
